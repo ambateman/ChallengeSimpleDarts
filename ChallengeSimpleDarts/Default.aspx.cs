75e5a20c410c5fe915d05926e457a934bce51149
ambateman <tmbateman@hotmail.com>
2017-10-19 19:45:23 -0700

    This is the simple darts challenge code

---
+++ ChallengeSimpleDarts/Default.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DartBoard;
+
+
+namespace ChallengeSimpleDarts
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Random myLuckyDay = new Random();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Darts DartGame = new Darts(myLuckyDay);
+                Game myGame = new Game(DartGame);
+                myGame.PlayDarts();  //Actually call the game.
+                fillResultLabel(myGame);
+                
+            }
+        }
+
+        private void fillResultLabel(Game thisGame)
+        {
+            //in case of tie:
+            if(thisGame.score1 == thisGame.score2)
+            {
+                lblDartsResults.Text = String.Format(" We have a TIE between {0} and {1}!!!", thisGame.player1, thisGame.player2);
+            }
+            else if (thisGame.score1 > thisGame.score2)
+            {
+                lblDartsResults.Text = String.Format(" The WINNER is  {0} with a score of  {1}!", thisGame.player1, thisGame.score1);
+            }
+            else
+            {
+                lblDartsResults.Text = String.Format(" The WINNER is  {0} with a score of  {1}!", thisGame.player2, thisGame.score2);
+            }
+            lblDartsResults.Text += String.Format(" <br/> The final score for  {0} is:  {1}!", thisGame.player1, thisGame.score1);
+            lblDartsResults.Text += String.Format(" <br/> The final score for  {0} is:  {1}!", thisGame.player2, thisGame.score2);
+        }
+
+
+
+
+
+
+
+        public class Game
+        {
+            Darts Darts;
+            public string player1 = "Freddy";
+            public string player2 = "Yun-Mi";
+
+            public int score1 { get; set; } = 0;
+            public int score2 { get; set; } = 0;
+
+            public Game(Darts myDarts)
+            {
+                this.Darts = myDarts;
+            }
+
+            public void PlayDarts()
+            {
+                //enter loop that lets each player play (3 throws per play)
+                while(score1<300 && score2 < 300)
+                {
+                    for(int i = 1; i < 3; i++)
+                    {
+                        Darts.Throw();
+                        //send properties to scoring method
+                        score1 += scorrer();
+                    }
+                    for (int i = 1; i < 3; i++)
+                    {
+                        Darts.Throw();
+                        //send properties to scoring method
+                        score2+= scorrer();
+                    }
+                    
+                }
+
+                
+
+            }
+
+            private int scorrer()
+            {
+                if(Darts.Zone == 0)
+                {
+                    if (Darts.OuterRing == false) return 50;
+                    else return 25;
+                }
+                else
+                {
+                    if (Darts.InnerRing == false && Darts.OuterRing == false) return Darts.Zone;
+                    if (Darts.InnerRing == true) return Darts.Zone * 3;
+                    if (Darts.OuterRing == true) return Darts.Zone * 2;
+                }
+
+                return 0;  //This line should never be reached.
+            }
+
+            private void outputLabel()
+            {
+                if(score1==score2)
+                {
+               
+
+
+
+                }
+
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/Darts.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartBoard
+{
+    public class Darts
+    {
+        private Random chaosTheory;
+        public int Zone { get; set; }
+        public bool InnerRing { get; set; }
+        public bool OuterRing { get; set; }
+
+
+
+        public Darts(Random objRandom)
+        {
+            this.chaosTheory = objRandom;
+        }
+
+        public Darts()
+        {
+            //empty constructor
+        }
+
+        public void Throw()
+        {
+            //Make sure our booleans are set to false to start
+            OuterRing = false;
+            InnerRing = false;
+            //Now, get zone. Zone 0 is the bullseye
+            Zone = chaosTheory.Next(0, 20);
+            //Compute ringage... note that if any single number has equal probabiliy
+            //then ANY number between 1 and 20 has a 5 percent chance of making it.
+            if (chaosTheory.Next(1, 20) == 5) OuterRing = true;  //rather than choose '1'
+            if (chaosTheory.Next(1, 20) == 7) InnerRing = true;  //rather than reuse first choice.
+            //for zone 0 (bullseye) the inner ring is alway false:
+            //hitting the inner bullseye is just a matter of having zone 0 and outerring=false.
+            if (Zone == 0) InnerRing = false;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
